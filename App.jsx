700611347752859c45b6a521dbff92fc3e3d72c0
MarinaNam <namma123@naver.com>
2023-01-01 02:04:29 +0900

    feat: addTodo, setCategory, setDone 구현

--- App.jsx
+++ App.jsx
@@ -11,11 +11,12 @@ import {
 import { MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
+import { useState } from 'react';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
-  const [category, setcategory] = useState('');
+  const [category, setCategory] = useState('');
   const [editText, setEditText] = useState('');
 
   // add Todo - 자료구조짜는 것이 일순위
@@ -27,42 +28,100 @@ export default function App() {
     category,
   };
   // addTodo 함수 만들기 & todos 출력하기
+  const addTodo = () => {
+    setTodos((prev) => [...prev, newTodo]);
+    setText('');
+  };
 
+  const setDone = (id) => {
+    // id를 매개변수로 받고
+    // setTodos를 위해 newTodos로 얕은 복사
+    // 선택된 todo의 index를 idx로 새로 선언
+    // todos[idx]의 isDone값을 !해서 토글링
+    // setTodos에 newTodos 저장
+    const newTodos = [...todos];
+    const idx = newTodos.findIndex((todo) => todo.id === id);
+    newTodos[idx].isDone = !newTodos[idx].isDone;
+    setTodos(newTodos);
+  };
   // deleteTodo 함수 만들기
+  // const deleteTodo = () => {
+  //   setTodos((todos.id)=>{})
+  // }
 
   // editTodo 함수 만들기
   return (
     <SafeAreaView style={styles.safearea}>
       <StatusBar style='auto' />
       <View style={styles.tabs}>
-        <TouchableOpacity style={styles.tab}>
+        {/* setCategory: tab 별로 다른 todos.category */}
+        <TouchableOpacity
+          onPress={() => setCategory('js')}
+          style={{
+            ...styles.tab,
+            backgroundColor: category === 'js' ? '#0FBCF9' : 'gray',
+          }}
+        >
           <Text>javascript</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.tab}>
+        <TouchableOpacity
+          onPress={() => setCategory('react')}
+          style={{
+            ...styles.tab,
+            backgroundColor: category === 'react' ? '#0FBCF9' : 'gray',
+          }}
+        >
           <Text>react</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.tab}>
-          <Text>ct</Text>
+        <TouchableOpacity
+          onPress={() => setCategory('ct')}
+          style={{
+            ...styles.tab,
+            backgroundColor: category === 'ct' ? '#0FBCF9' : 'gray',
+          }}
+        >
+          <Text>coding test</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.inputWrapper}>
         <TextInput
           style={styles.input}
           placeholder='오늘 할 일을 입력하세요.'
+          onSubmitEditing={addTodo}
+          onChangeText={setText}
+          value={text}
         />
       </View>
       <ScrollView>
-        <View style={styles.task}>
-          <TouchableOpacity>
-            <MaterialIcons name='edit' size={24} color='black' />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome name='check-circle' size={24} color='black' />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Feather name='delete' size={24} color='black' />
-          </TouchableOpacity>
-        </View>
+        {/* 투두스에서 하나씩 뽑은 투두 출력 */}
+        {todos.map((todo) => {
+          // 카테고리 if문으로 분류
+          if (category === todo.category) {
+            return (
+              <View key={todo.id} style={styles.task}>
+                <Text
+                  style={{
+                    textDecorationLine: todo.isDone ? 'line-through' : 'none',
+                  }}
+                >
+                  {todo.text}
+                </Text>
+                <View style={{ flexDirection: 'row' }}>
+                  {/* setDone 완료 토글링 함수 적용 */}
+                  <TouchableOpacity onPress={() => setDone(todo.id)}>
+                    <FontAwesome name='check-circle' size={24} color='black' />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <MaterialIcons name='edit' size={24} color='black' />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Feather name='delete' size={24} color='black' />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            );
+          }
+        })}
       </ScrollView>
     </SafeAreaView>
   );
